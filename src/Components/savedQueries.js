aecf32c1c1ad9e2ed01130ba329e2106e570d8cd
Gurmanjot Singh Randhawa <gurmanjot.randhawa@razorpay.com>
2024-02-21 09:36:02 +0530

    chore: minor change

--- src/Components/savedQueries.js
+++ src/Components/savedQueries.js
@@ -18,7 +18,7 @@ const SavedQueries = () => {
   }, []);
 
   const deleteSavedQuery = (query) => {
-    const newQueries = savedQueries.filter(
+    const newQueries = savedQueries?.filter(
       (savedQuery) => savedQuery !== query
     );
     localStorage.setItem("queries", JSON.stringify(newQueries));
@@ -32,61 +32,69 @@ const SavedQueries = () => {
       </Typography>
       <Box height={"160px"} overflow={"auto"}>
         {savedQueries?.length !== 0 ? (
-          <Input
-            value={searchSavedQueries}
-            onChange={(event) => {
-              setSearchSavedQueries(event.target.value);
-            }}
-            placeholder={"Search saved queries"}
-            sx={{
-              border: "0.5px solid black",
-              marginY: "5px",
-              padding: "5px",
-              width: "100%",
-              height: "30px",
-            }}
-          />
+          <>
+            <Input
+              value={searchSavedQueries}
+              onChange={(event) => {
+                setSearchSavedQueries(event.target.value);
+              }}
+              placeholder={"Search saved queries"}
+              sx={{
+                border: "0.5px solid black",
+                marginY: "5px",
+                padding: "5px",
+                width: "100%",
+                height: "30px",
+              }}
+            />
+            {savedQueries
+              ?.filter((query) =>
+                searchSavedQueries
+                  ? query
+                      .toLowerCase()
+                      .includes(searchSavedQueries.toLowerCase())
+                  : true
+              )
+              ?.map((query, index) => (
+                <Box
+                  key={index}
+                  display={"flex"}
+                  flexDirection={"row"}
+                  alignItems={"center"}
+                  justifyContent={"space-between"}
+                  borderBottom={"1px solid black"}
+                  paddingY={"5px"}
+                  textAlign={"left"}
+                >
+                  <Typography variant={"body2"}>{query}</Typography>
+                  <Box
+                    display={"flex"}
+                    flexDirection={"row"}
+                    marginLeft={"10px"}
+                  >
+                    <Box sx={{ marginRight: "10px" }}>
+                      <ContentCopyRoundedIcon
+                        color="info"
+                        onClick={() => copyToClipboard(query)}
+                        cursor={"pointer"}
+                      />
+                    </Box>
+                    <Box>
+                      <DeleteOutlineRoundedIcon
+                        color="error"
+                        onClick={() => deleteSavedQuery(query)}
+                        cursor={"pointer"}
+                      />
+                    </Box>
+                  </Box>
+                </Box>
+              ))}
+          </>
         ) : (
           <Box mt={"40px"}>
             <Typography color={"GrayText"}>No queries to show...</Typography>
           </Box>
         )}
-        {savedQueries
-          ?.filter((query) =>
-            searchSavedQueries
-              ? query.toLowerCase().includes(searchSavedQueries.toLowerCase())
-              : true
-          )
-          .map((query, index) => (
-            <Box
-              key={index}
-              display={"flex"}
-              flexDirection={"row"}
-              alignItems={"center"}
-              justifyContent={"space-between"}
-              borderBottom={"1px solid black"}
-              paddingY={"5px"}
-              textAlign={"left"}
-            >
-              <Typography variant={"body2"}>{query}</Typography>
-              <Box display={"flex"} flexDirection={"row"} marginLeft={"10px"}>
-                <Box sx={{ marginRight: "10px" }}>
-                  <ContentCopyRoundedIcon
-                    color="info"
-                    onClick={() => copyToClipboard(query)}
-                    cursor={"pointer"}
-                  />
-                </Box>
-                <Box>
-                  <DeleteOutlineRoundedIcon
-                    color="error"
-                    onClick={() => deleteSavedQuery(query)}
-                    cursor={"pointer"}
-                  />
-                </Box>
-              </Box>
-            </Box>
-          ))}
       </Box>
     </Box>
   );
